0f5869a8586935070c299c8c04461b0700aef381
Srijan <srijanspl2017@gmail.com>
2026-03-21 11:51:35 +0530

    PDF parsing issue resolved

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse"],
+  /* config options here */
 };
 
 export default nextConfig;
+
